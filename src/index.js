e8e9cf665084f174b2a65c70367771be6f2c32df
azad <mohammadazad.bc~gmail.com>
2021-10-12 11:07:54 +0200

    refactor with json package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-const menuBtn = document.getElementById('menu-btn');
-const menuBar = document.getElementById('menu-bar')
-const closeBtn = document.getElementById('close-menu')
-const slider = document.getElementById('slider')
+import { ShowMenu } from "./function";
+import { IncreAndDecrse } from "./function";
+import { deleteItem } from "./function";
+import { removeAlert } from "./function";
+
 const shoppingIcon = document.getElementById('shopping-icon')
 const basket = document.getElementById('shop-basket')
-const incress = document.getElementById('incress');
-const decress = document.getElementById('decrese')
 const addToCart = document.getElementById('add-to-cart');
 const price = document.getElementById('price')
 let netPrice =document.getElementById('net-price').value;
@@ -13,77 +12,27 @@ const basketBody = document.getElementById('basket-body')
 const empty = document.querySelector('.empty')
 const modelName = document.getElementById('modelName')
 const qteNull = document.getElementById('qte-null');
-const ok = document.getElementById('ok')
 
-
-
-
-function ShowMenu(){
-    hideMenu();
-    // show the menu bar 
-    menuBtn.addEventListener('click',()=>{
-        menuBar.classList.add('show')
-        document.body.classList.add('dark')
-    })
-
-
-}
-ShowMenu();
-
-
-function hideMenu(){
-    // close the menu bar
-    closeBtn.addEventListener('click', ()=>{
-        menuBar.classList.remove('show')
-        document.body.classList.remove('dark')
-    })
-
-}
 
 
 shoppingIcon.addEventListener('click', ()=>{
     basket.classList.toggle('show')
 })
 
-// -------------------------------------------
-// Incresse and decrese the the qte of shose
-//--------------------------------------------
+ShowMenu()
 
-function IncreAndDecrse(){
-let i = 0;
-decress.addEventListener('click',()=>{
-if(i>0){
-    i --;
-    document.getElementById('qte').value = i;
-}
-else{
-    document.getElementById('qte').value =0;
-}
-})
-
-incress.addEventListener('click',()=>{
-    i ++;
-    document.getElementById('qte').value =i;
-})
-
-}
 IncreAndDecrse();
-
-//---------------------------------------------
-//---------------------------------------------
 
 // add article to shoping basket
 
-
+function addArticleInCart(){
 
 addToCart.addEventListener('click',()=>{
-
     if(document.getElementById('qte').value ==0){
         qteNull.classList.add('on')
     }
     else{
-        
-        
+         
         empty.classList.add('hide')
         
         const basketDiv = document.createElement('div')
@@ -120,11 +69,7 @@ addToCart.addEventListener('click',()=>{
         deletIcon.className="delete-icon"
         deleteItem(deletIcon)
        
-       
-        // deletIcon.addEventListener('click', ()=>{
-         //   deletIcon.parentElement.parentElement.remove();
-           
-       // })
+
         firstDiv.appendChild(deletIcon)
         
         const checkOutBtn = document.createElement('button');
@@ -139,46 +84,8 @@ addToCart.addEventListener('click',()=>{
 
 
 })
-
-ok.addEventListener('click',()=>{
-    qteNull.classList.remove('on')
-})
-
-
-
-
-function deleteItem(icon){
-    icon.addEventListener('click', ()=>{
-        icon.parentElement.parentElement.remove();
-    })
 }
 
+addArticleInCart();
+removeAlert();
 
-
-/*
-
-
-let images  =['img1.jpg', 'img2.jpg','img3.jpg','img4.jpg'];
-let i = 0;
-let time = 2000;
-
-
-function changeImg(){
-document.slide.src =images[i];
-
-    if(i<images.length-1){
-      i ++;
-    }
-    else{
-     i =0;
-    }
-    setTimeout("changeImg()", time)
-    }
-    window.onload = changeImg();
-
-
-
-
-
-
-*/
